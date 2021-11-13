46e2f29648ee97d6314f5dff4b5981cf70bcd6c8
Intezar Ahmad <31982418+theintezar@users.noreply.github.com>
2021-11-13 14:07:25 +0530

    adding my work

---
+++ component/navbar.js
@@ -0,0 +1,21 @@
+function navbar() {
+    return `
+     <div class="navbar">
+    <h3><a href=""><img class="logo" src="https://static.frontendmasters.com/assets/fm/js/static/frontendmasters.e1e10b58c1.svg" alt=""></a></h3>
+<div id="options">
+<h3><a style="text-decoration: none;" href=""><p class="nvtxt">Home</p> </a></h3>
+<h3><a style="text-decoration: none;" href=""><p class="nvtxt">Courses</p> </a></h3>
+<h3><a style="text-decoration: none;"href=""><p class="nvtxt">Learn</p></a></h3>
+<h3><a style="text-decoration: none;"href=""><p class="nvtxt">Workshpop</p></a></h3>
+<h3><a style="text-decoration: none;"href=""><p class="nvtxt">My Account</p></a></h3>
+<h3><a style="text-decoration: none;"href=""><p class="nvtxt">Logout</p></a></h3>
+
+</div>
+</div>
+
+<svg class="diagonal" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 10" preserveaspectratio="none"><defs><linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#c02d28"></stop><stop offset="100%" stop-color="#e66225"></stop></linearGradient></defs><polygon points="0,2 0,0 100,0 100,10" fill="url(#linear)"></polygon></svg>`
+}
+
+
+
+export default navbar ;
